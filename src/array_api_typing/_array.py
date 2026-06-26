eb9072750067db7d80ff41ec4a051563a9ed0bfc
Lucas Colley <lucas.colley8@gmail.com>
2026-06-26 21:59:17 +0100

    Update src/array_api_typing/_array.py
    Signed-off-by: Lucas Colley <lucas.colley8@gmail.com>

--- src/array_api_typing/_array.py
+++ src/array_api_typing/_array.py
@@ -14,8 +14,6 @@ DTypeT_co = TypeVar("DTypeT_co", covariant=True)
 class HasArrayNamespace(Protocol[NamespaceT_co]):
     """Protocol for classes that have an `__array_namespace__` method.
 
-    See the Array API specification: https://data-apis.org/array-api/latest/
-
     This `Protocol` is intended for use in static typing to ensure that an
     object has an `__array_namespace__` method that returns a namespace for
     array operations. This `Protocol` should not be used at runtime for type
